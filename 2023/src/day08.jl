329d295f94b3d4771765cfa90ac9c09361163c44
Andrew Jeffery <dev@jeffas.io>
2023-12-08 19:25:37 +0000

    Well, with a bit of helpful insight that turns out to be stupid

--- 2023/src/day08.jl
+++ 2023/src/day08.jl
@@ -52,12 +52,11 @@ function part2_inner(input)
         graph[root] = (l, r)
     end
     current_nodes = [c for c in keys(graph) if endswith(c, "A")]
-    @show current_nodes
     letter = 0
+    loop_lengths = []
     for current_node in current_nodes
         iterations = 0
         path = Dict()
-        # @show current_node
 
         start_to_end_of_loop = 0
         start_of_loop_to_z = 0
@@ -73,21 +72,17 @@ function part2_inner(input)
             if endswith(current_node, "Z")
                 push!(zs, (lrs_offset, current_node, iterations))
             end
-            # @show lrs_offset, old_node, lr
             if haskey(path, (lrs_offset, old_node))
                 if start_to_end_of_loop == 0
                     start_to_end_of_loop = iterations
-                    # @show start_to_end_of_loop
                 else
                     if endswith(current_node, "Z")
                         if start_of_loop_to_z == 0
                             start_of_loop_to_z = iterations - start_to_end_of_loop
-                            # @show start_of_loop_to_z
                         else
                             # already found the z, and just found it again
                             loop_length = iterations - start_to_end_of_loop - start_of_loop_to_z
                             start_to_start_of_loop = start_to_end_of_loop - loop_length
-                            # @show loop_length
                             break
                         end
                     end
@@ -105,22 +100,17 @@ function part2_inner(input)
             end
             path[(lrs_offset, old_node)] = current_node
         end
-        # @show path
-        # @show start_to_end_of_loop, start_of_loop_to_z, loop_length, start_to_start_of_loop
-        # # check that there are only one z per loop
-        # for (lr, z, i) in zs
-        #     @show z,lr, i,  (i-start_to_start_of_loop)% loop_length
-        # end
         l = letter + 'a'
         letter += 1
-        println("y = $start_to_start_of_loop + $start_of_loop_to_z + $l * $loop_length")
+        # println("y = $start_to_start_of_loop + $start_of_loop_to_z + $l * $loop_length")
+        # println("y = $(start_to_start_of_loop + start_of_loop_to_z) + $l * $loop_length")
+        # println("y = ($l + 1) * $loop_length")
+        push!(loop_lengths, loop_length)
     end
 
-    # a = start to loop start
-    # z = loop start to z
-    # l = loop length
-    # iterations = a + z + xl
-    0
+    # turns out that the input is forced to be a not very general pattern.
+    # turns into each loop being at the start and going its length, then just finding the lcm
+    lcm(loop_lengths...)
 end
 
 
@@ -154,6 +144,6 @@ ZZZ = (ZZZ, ZZZ)
 22C = (22Z, 22Z)
 22Z = (22B, 22B)
 XXX = (XXX, XXX)") == 6
-    # @test Day08.part2() == 253907829
+    @test Day08.part2() == 14321394058031
 end
 end
